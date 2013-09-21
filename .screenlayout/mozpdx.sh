31787fc8fae257fba381f94034b2e52095df2596
Christie Koehler <christiekoehler@gmail.com>
2013-09-20 21:45:12 -0700

    adding screenlayouts

---
+++ .screenlayout/mozpdx.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP3 --off --output DP2 --off --output DP1 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --off
